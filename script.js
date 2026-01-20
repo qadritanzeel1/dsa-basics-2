d8ed2d760b32f65b3d0df860c8cdd02095c66a55
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-01-20 10:33:05 +0530

    DSA basics - sum of two integers

---
+++ script.js
@@ -0,0 +1,18 @@
+//Starting DSA from complete basics 
+
+//sum of two integers 
+
+let a=10;
+let b =20;
+
+console.log(a+b)
+
+// we can also do like this 
+let d=10;
+let e =20;
+let sum = a+b
+
+console.log(sum)
+
+// also we can write like this 
+
